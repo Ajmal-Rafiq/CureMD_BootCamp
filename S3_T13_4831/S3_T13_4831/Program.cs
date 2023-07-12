a49a0d94cc2ba01b2512e63ce8252ef445c7ea73
Ajmal-Rafiq <106944365+Ajmal-Rafiq@users.noreply.github.com>
2023-07-12 07:10:40 -0400

    Add files via upload

---
+++ S3_T13_4831/S3_T13_4831/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace S3_T13_4831
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Calculating The Largest Number and Smallest in An Array");
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------------------------------");
+            int[] array = { 3, 5, 7, 2, 8, 9,1 };
+            int len = array.Length;
+            int minValue = int.MaxValue;  // Initialize minValue to maximum possible value
+            int maxValue = int.MinValue;  // Initialize maxValue to minimum possible value
+
+            for (int i = 0; i < len; i++)
+            {
+                if (array[i] < minValue)
+                {
+                    minValue = array[i];
+                }
+
+                if (array[i] > maxValue)
+                {
+                    maxValue = array[i];
+                }
+            }
+
+            Console.WriteLine("Min Value of Array: " + minValue);
+            Console.WriteLine("Max Value of Array: " + maxValue);
+
+
+
+        }
+    }
+}
