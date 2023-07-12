6249b835e7228f9025ff0849416ae353a869a28e
Ajmal-Rafiq <106944365+Ajmal-Rafiq@users.noreply.github.com>
2023-07-12 07:17:11 -0400

    Add files via upload

---
+++ S4_T20_4831/S4_T20_4831/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace S4_T20_4831
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "ABCDGH";
+            string str2 = "AEDFHR";
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+
+            }
+        }
+    }
+}
