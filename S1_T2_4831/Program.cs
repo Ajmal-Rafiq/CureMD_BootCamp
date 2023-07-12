07b7c877d6011fa463209f130d4a196cfc88b618
Ajmal-Rafiq <106944365+Ajmal-Rafiq@users.noreply.github.com>
2023-07-12 07:08:39 -0400

    Add files via upload

---
+++ S1_T2_4831/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace S1_T2_4831
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NameGreeting();//Calling Func
+        }
+
+        static void NameGreeting()
+        {
+            Console.WriteLine("Enter Your Name: ");
+            string name = Console.ReadLine();//taking input from the user 
+            Console.WriteLine("Hello! " + name); // printing greeting with name 
+
+        }
+
+    }
+}
+
---
+++ S1_T3_4831/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace S1_T3_4831
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum();// calling the sum function
+        }
+
+
+        static void Sum() 
+        {
+            int first_number;
+            int second_number;
+            Console.WriteLine("Enter the First Digit");
+            first_number = Convert.ToInt32(Console.ReadLine());//taking user input and converting it into intteger
+            Console.WriteLine("Enter the Second Digit");
+            second_number = Convert.ToInt32(Console.ReadLine());//taking user input and converting it into intteger
+            int sum = first_number + second_number;//doing the sum of user entered numbers
+            Console.WriteLine("Sum of the Number is :" + sum);// print the sum of the numbers
+            
+        } 
+    }
+}
